7b803d3e100656df6365829a52a79144ec9dc1b3
Mike Grissom <lmgrissom@gmail.com>
2017-06-08 13:18:30 -0700

    api call that works, plus travis

--- glassy.gemspec
+++ glassy.gemspec
@@ -13,6 +13,8 @@ Gem::Specification.new do |spec|
   spec.description   = %q{Get alerts when it's gonna be glassy!}
   spec.require_paths = ['lib']
 
+  spec.add_runtime_dependency 'json'#, '~> 1.8.7'
+
   spec.add_development_dependency 'bundler', '~> 1.10.6'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
--- lib/glassy.rb
+++ lib/glassy.rb
@@ -1,7 +1,19 @@
 require 'glassy/version'
+require 'pp'
+require 'json'
 
 module Glassy
   class Location
+
+    OPENWEATHERMAP_TOKEN = ENV['OPENWEATHERMAP_TOKEN']
+    OPENWEATHERMAP_HOST = 'http://api.openweathermap.org'
+
+    def initialize(latitude, longitude)
+      response = `curl -s "#{OPENWEATHERMAP_HOST}/data/2.5/weather?APPID=#{OPENWEATHERMAP_TOKEN}&lat=#{latitude}&lon=#{longitude}"`
+      json = JSON.parse(response)
+      p json
+    end
+
     def self.where?
       p 'Here!'
     end
