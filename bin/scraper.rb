7bdbe524b348ed4f3f68580fdaf4649053074e44
Mike Grissom <lmgrissom@gmail.com>
2017-07-22 15:48:42 -0700

    add quality to scraper data

--- bin/scraper.rb
+++ bin/scraper.rb
@@ -4,24 +4,27 @@ require 'json'
 require 'csv'
 require 'active_support/time'
 
-OUTPUT_FILE = '/home/pi/surfline_data.txt'
+OUTPUT_FILE = '/home/pi/swamis.tsv'
 
 swamis = 'https://new.surfline.com/surf-report/swami-s/5842041f4e65fad6a77088b4/forecast'
 swamis_html = HTTParty.get(swamis)
 parsed_html = Nokogiri::HTML(swamis_html)
-current_time = Time.now
+
 report_time_text = parsed_html.css('.quiver-forecaster-profile__update-text').text
 report_time = nil
-if report_time_text.match /hours/
+
+if report_time_text.match /hour/
   hours = report_time_text.gsub(/[^0-9]/, '').to_i
   report_time = Time.now - hours.hours
 else
-  abort
+  raise ParseError
 end
 
+quality = parsed_html.css('.sl-spot-rail-header__conditions').text
 surf_height =  parsed_html.css('.quiver-surf-height').text
 reporter = parsed_html.css('.quiver-forecaster-profile__forecaster-name').text
 report_text = parsed_html.css('.sl-spot-report__text__body').text.gsub(/\s+/, ' ')
+
 File.open(OUTPUT_FILE, 'a') do |f|
-  f << "#{report_time}\t#{reporter}\t#{surf_height}\t#{report_text}\n"
+  f << "#{report_time}\t#{reporter}\t#{quality}\t#{surf_height}\t#{report_text}\n"
 end
